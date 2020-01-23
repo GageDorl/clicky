c6a847b0ffe615c9b50b319fada371153e6e878e
Gage D'Orlando <gagedorlando@gmail.com>
2020-01-23 14:14:46 -0500

    Updates

---
+++ precache-manifest.fb6a3fdd7baabe1b06612d148d954920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36af79b1c54ba6c30273280fa8bc367",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "a48d8bb267581f5767e1",
+    "url": "/clicky/static/css/main.b508ac42.chunk.css"
+  },
+  {
+    "revision": "45ffbf9413bf15656efe",
+    "url": "/clicky/static/js/2.28da03a2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky/static/js/2.28da03a2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a48d8bb267581f5767e1",
+    "url": "/clicky/static/js/main.f80dbfb5.chunk.js"
+  },
+  {
+    "revision": "03845033a8934ae5952d",
+    "url": "/clicky/static/js/runtime-main.74279dd7.js"
+  }
+]);
